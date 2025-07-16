5aaf7422979379f10dd06691ba35a462923cc82d
coder-nitin07 <nitinthakur87504@gmail.com>
2025-07-16 18:36:09 +0530

    Get all meal logs for logged-in user

--- Backend/controllers/mealController.js
+++ Backend/controllers/mealController.js
@@ -1,3 +1,4 @@
+const User = require("../models/authSchema");
 const MealLog = require("../models/mealSchema");
 
 // Meal Data
@@ -28,4 +29,23 @@ const mealLog = async (req, res)=>{
     }
 };
 
-module.exports = { mealLog };
+// Get Logs
+const getAllLogs = async (req, res)=>{
+    try {
+        const userId = req.user.id;
+        const getUser = await User.findById( userId );
+
+        if(!getUser){
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
+        const getLogs = await MealLog.find({ user: userId }).sort({ createdAt: -1 });
+
+        res.status(200).json({ message: 'Successfully Fetched All Logs', logs: getLogs });
+    } catch (err) { 
+        console.log(err);
+        res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
+module.exports = { mealLog, getAllLogs };
--- Backend/routes/mealRoutes.js
+++ Backend/routes/mealRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { mealLog } = require('../controllers/mealController');
+const { mealLog, getAllLogs } = require('../controllers/mealController');
 const mealValidation = require('../middlewares/mealValidation');
 const blacklistToken = require('../middlewares/blacklistToken');
 const mealRouter = express.Router();
 
 mealRouter.post('/meal-log', blacklistToken, mealValidation, mealLog);
+mealRouter.get('/getAllLogs', blacklistToken, getAllLogs);
 
 module.exports = mealRouter;
