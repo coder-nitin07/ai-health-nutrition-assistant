17d400d4b6e460becf08409c4dde235c0674b2b6
coder-nitin07 <nitinthakur87504@gmail.com>
2025-07-16 14:37:17 +0530

    Create Login API

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createUser } = require('../controllers/authController');
+const { createUser, loginUser } = require('../controllers/authController');
 const authValidation = require('../middlewares/authValidation');
+const validateLogin = require('../middlewares/authLogin');
 const authRouter = express.Router();
 
 authRouter.post('/register', authValidation, createUser);
+authRouter.post('/login', validateLogin, loginUser);
 
 module.exports = authRouter;
