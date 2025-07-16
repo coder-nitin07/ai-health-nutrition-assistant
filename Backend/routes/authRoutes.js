7c54b2b684eb44c5434c2fabecedcbb720b2ee76
coder-nitin07 <nitinthakur87504@gmail.com>
2025-07-16 15:17:42 +0530

    Create Logout API

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUser, loginUser } = require('../controllers/authController');
+const { createUser, loginUser, logoutUser } = require('../controllers/authController');
 const authValidation = require('../middlewares/authValidation');
 const validateLogin = require('../middlewares/authLogin');
+const blacklistToken = require('../middlewares/blacklistToken');
 const authRouter = express.Router();
 
 authRouter.post('/register', authValidation, createUser);
 authRouter.post('/login', validateLogin, loginUser);
+authRouter.post('/logout', logoutUser);
 
 module.exports = authRouter;
